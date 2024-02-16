ab003d048dc1b6cd9de02138a13d8185a78e5404
ayushrai6249 <ayushrai6249@gmail.com>
2024-02-16 21:50:13 +0530

    Add files to github

---
+++ app.js
@@ -0,0 +1,70 @@
+let btn = document.querySelector('button');
+let inp = document.querySelector('input');
+let ul = document.querySelector('ul');
+
+btn.addEventListener("click", function () {
+    let item = document.createElement('li');
+    item.innerText = inp.value;
+
+    let dltBtn = document.createElement('button');
+    dltBtn.innerText = "Remove Task";
+    dltBtn.classList.add('delete');
+
+    if (item.innerText == "") {
+        alert("Add something");
+    }
+    else {
+        ul.appendChild(item);
+        item.appendChild(dltBtn);
+    }
+    inp.value = "";
+});
+
+inp.addEventListener("keydown", function (Event) {
+    if (Event.code == "Enter" || Event.code == "NumpadEnter") {
+        let item = document.createElement('li');
+        item.innerText = inp.value;
+
+        let dltBtn = document.createElement('button');
+        dltBtn.innerText = "Remove Task";
+        dltBtn.classList.add('delete');
+
+        if (item.innerText == "") {
+            alert("Add something");
+        }
+        else {
+            ul.appendChild(item);
+            item.appendChild(dltBtn);
+        }
+        inp.value = "";
+    }
+})
+
+ul.addEventListener("click", function (Event) {
+    if (Event.target.nodeName == "BUTTON") {
+        let itemList = Event.target.parentElement;
+        itemList.remove();
+    }
+})
+
+let p = document.querySelector('.sentence');
+
+// This is used to make webpage static when keaboard popup in mobiles
+
+let originalHeight = window.innerHeight;
+
+window.addEventListener('resize', () => {
+    if (window.innerHeight < originalHeight) {
+        // Keyboard is open
+        document.body.style.height = originalHeight + 'px';
+        p.remove();
+    } else {
+        // Keyboard is closed
+        document.body.style.height = '100vh';
+    }
+});
+
+// Following event listener to handle orientation changes
+window.addEventListener('orientationchange', () => {
+    originalHeight = window.innerHeight;
+});
